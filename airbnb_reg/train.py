43f00724f29e9fd39d1913dbf404341ffd617d48
azmonoam <azmonoam@gmail.com>
2022-03-04 11:38:57 +0200

    cleaning

--- airbnb_reg/train.py
+++ airbnb_reg/train.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import torch
 import time
-import utils2
 import argparse
 import os
 import matplotlib.pyplot as plt
@@ -19,7 +18,7 @@ import datetime
 parser = argparse.ArgumentParser(description='airbnb_reg: Photo album Event recognition using Transformers Attention.')
 parser.add_argument('--model_path', type=str, default='./models_local/peta_32.pth')
 parser.add_argument('--album_path', type=str, default='/home/labs/testing/class63/airbnb')
-parser.add_argument('--val_dir', type=str, default='/home/labs/testing/class63/airbnb')  # /Graduation') # /0_92024390@N00')
+parser.add_argument('--val_dir', type=str, default='/home/labs/testing/class63/airbnb')
 parser.add_argument('--num_classes', type=int, default=1)
 parser.add_argument('--model_name', type=str, default='mtresnetaggregate')
 parser.add_argument('--transformers_pos', type=int, default=1)
@@ -101,7 +100,6 @@ def main():
     idx_to_class = {i: i for i in range(16)}
     idx_to_class.update({i: i+1 for i in range(16, 20)})
     classes_list = np.array(list(idx_to_class.values()))
-    # print('Class list:', classes_list)
 
     # Setup data loader
     print('creating data loader...')
@@ -161,128 +159,6 @@ def main():
     # print("final validation map: {:.2f}".format(map))
 
     print('Done\n')
-
-
-from utils2 import Metric, accuracy
-
-__all__ = ['test_epoch', 'test_epoch', 'train_loop']
-
-
-#################################################
-# train_epoch
-#################################################
-
-def train_epoch(model, criterion, optimizer, loader, device):
-    """Trains over an epoch, and returns the accuracy and loss over the epoch.
-
-  Note: The accuracy and loss are average over the epoch. That's different from
-  running the classifier over the data again at the end of the epoch, as the
-  weights changed over the iterations. However, it's a common practice, since
-  iterating over the training set (again) is time and resource exhustive.
-
-  Note: You MUST have `loss` tensor with the loss value, and `acc` tensor with
-  the accuracy (you can use the imported `accuracy` method).
-
-  Args:
-    model (torch.nn.Module): The model.
-    criterion (callable): The loss function. Should return a scalar tensor.
-    optimizer (torch.optim.Optimizer): The optimizer.
-    loader (torch.utils.data.DataLoader): The test set data loader.
-    device (torch.device): The device to run on.
-
-  Returns:
-    acc_metric (Metric): The accuracy metric over the epoch.
-    loss_metric (Metric): The loss metric over the epoch.
-  """
-    loss_metric = Metric()
-    acc_metric = Metric()
-    for x, y in loader:
-        x, y = x.to(device=device), y.to(device=device)
-        # BEGIN SOLUTION
-        # raise NotImplementedError
-        x = x.squeeze()
-        x = torch.reshape(x, (x.size()[0], x.size()[1] * x.size()[2]))
-        pred = model(x)
-        loss = criterion(pred, y)
-        acc = accuracy(pred, y)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # END SOLUTION
-        loss_metric.update(loss.item(), x.size(0))
-        acc_metric.update(acc.item(), x.size(0))
-    return loss_metric, acc_metric
-
-
-#################################################
-# test_epoch
-#################################################
-
-def test_epoch(model, criterion, loader, device):
-    """Evaluating the model at the end of the epoch.
-
-  Note: You MUST have `loss` tensor with the loss value, and `acc` tensor with
-  the accuracy (you can use the imported `accuracy` method).
-
-  Args:
-    model (torch.nn.Module): The model.
-    criterion (callable): The loss function. Should return a scalar tensor.
-    loader (torch.utils.data.DataLoader): The test set data loader.
-    device (torch.device): The device to run on.
-
-  Returns:
-    acc_metric (Metric): The accuracy metric over the epoch.
-    loss_metric (Metric): The loss metric over the epoch.
-  """
-    loss_metric = Metric()
-    acc_metric = Metric()
-    for x, y in loader:
-        x, y = x.to(device=device), y.to(device=device)
-        # BEGIN SOLUTION
-        # raise NotImplementedError
-        x = x.squeeze()
-        x = torch.reshape(x, (x.size()[0], x.size()[1] * x.size()[2]))
-        pred = model(x)
-        loss = criterion(pred, y)
-        acc = accuracy(pred, y)
-        # END SOLUTION
-        loss_metric.update(loss.item(), x.size(0))
-        acc_metric.update(acc.item(), x.size(0))
-    return loss_metric, acc_metric
-
-
-#################################################
-# PROVIDED: train_loop
-#################################################
-
-def train_loop(model, criterion, optimizer, train_loader, test_loader, device, epochs, test_every=1):
-    """Trains a model to minimize some loss function and reports the progress.
-
-  Args:
-    model (torch.nn.Module): The model.
-    criterion (callable): The loss function. Should return a scalar tensor.
-    optimizer (torch.optim.Optimizer): The optimizer.
-    train_loader (torch.utils.data.DataLoader): The training set data loader.
-    test_loader (torch.utils.data.DataLoader): The test set data loader.
-    device (torch.device): The device to run on.
-    epochs (int): Number of training epochs.
-    test_every (int): How frequently to report progress on test data.
-  """
-    for epoch in range(1, epochs + 1):
-        train_loss, train_acc = train_epoch(model, criterion, optimizer, train_loader, device)
-        print('Train', f'Epoch: {epoch:03d} / {epochs:03d}',
-              f'Loss: {train_loss.avg:7.4g}',
-              f'Accuracy: {train_acc.avg:.3f}',
-              sep='   ')
-        if epoch % test_every == 0:
-            test_loss, test_acc = test_epoch(model, criterion, test_loader, device)
-            print(' Test', f'Epoch: {epoch:03d} / {epochs:03d}',
-                  f'Loss: {test_loss.avg:7.4g}',
-                  f'Accuracy: {test_acc.avg:.3f}',
-                  sep='   ')
-
 
 if __name__ == '__main__':
     main()
