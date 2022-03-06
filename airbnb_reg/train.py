fff6dd38d598c0f1bc591b4084bd69e3082a204a
michalgeyer <michal.geyer@gmail.com>
2022-03-06 13:23:45 +0200

    fixed album func

--- airbnb_reg/train.py
+++ airbnb_reg/train.py
@@ -54,7 +54,7 @@ def save_epochs_loss_results(now_ts, epoch, train_loss_data, test_loss_data, arg
     plt.plot(train_loss_grouped_data['epoch'], train_loss_grouped_data['loss'])
     plt.plot(test_loss_grouped_data['epoch'], test_loss_grouped_data['loss'])
     plt.legend(['train', 'test'])
-    plt.savefig('{}/losses/looses_{}_ep_{}.jpg'.format(args.results_path, now_ts, str(epoch)))
+    plt.savefig('{}/losses/losses_{}_ep_{}.jpg'.format(args.results_path, args.start_ts, str(epoch)))
 
 
 def create_album_list(train_val_loader):
@@ -66,7 +66,7 @@ def create_album_list(train_val_loader):
     for j in range(0, len(train_val_loader.dataset.samples), num_apt):
         id_list = []
         for i in range(0, num_apt):
-            id_list.append(train_val_loader.dataset.samples[i][0])
+            id_list.append(train_val_loader.dataset.samples[j+i][0])
         listings.append(id_list)
     for i in range(len(all_album_list)):
         all_album_list[i].append(listings[i])
