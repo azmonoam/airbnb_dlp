af976cd298cb05927e5eacbc8a6cb73d5a9bad21
michalgeyer <michal.geyer@gmail.com>
2022-03-06 11:48:23 +0200

    added and modified Tal's infer function

--- airbnb_reg/infer.py
+++ airbnb_reg/infer.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import torch
 import argparse
 import os
@@ -33,89 +34,77 @@ parser.add_argument('--threshold', type=float, default=0.85)
 parser.add_argument('--remove_model_jit', type=int, default=None)
 
 
-def get_album(args):
+#def get_album(args):
 
-    files = os.listdir(args.album_path)
-    n_files = len(files)
-    idx_fetch = np.linspace(0, n_files-1, args.album_clip_length, dtype=int)
-    tensor_batch = torch.zeros(len(idx_fetch), args.input_size, args.input_size, 3)
-    for i, id in enumerate(idx_fetch):
-        im = Image.open(os.path.join(args.album_path, files[id]))
-        im_resize = im.resize((args.input_size, args.input_size))
-        np_img = np.array(im_resize, dtype=np.uint8)
-        tensor_batch[i] = torch.from_numpy(np_img).float() / 255.0
-    tensor_batch = tensor_batch.permute(0, 3, 1, 2)#.cuda()   # HWC to CHW
+#    files = os.listdir(args.album_path)
+#    n_files = len(files)
+#    idx_fetch = np.linspace(0, n_files-1, args.album_clip_length, dtype=int)
+#    tensor_batch = torch.zeros(len(idx_fetch), args.input_size, args.input_size, 3)
+#    for i, id in enumerate(idx_fetch):
+#        im = Image.open(os.path.join(args.album_path, files[id]))
+#        im_resize = im.resize((args.input_size, args.input_size))
+#        np_img = np.array(im_resize, dtype=np.uint8)
+#        tensor_batch[i] = torch.from_numpy(np_img).float() / 255.0
+#    tensor_batch = tensor_batch.permute(0, 3, 1, 2)#.cuda()   # HWC to CHW
     # tensor_images = torch.unsqueeze(tensor_images, 0).cuda()
-    montage = torchvision.utils.make_grid(tensor_batch).permute(1, 2, 0).cpu()
-    return tensor_batch, montage
+#    montage = torchvision.utils.make_grid(tensor_batch).permute(1, 2, 0).cpu()
+#    return tensor_batch, montage
 
 
-def inference(tensor_batch, model, classes_list, args):
+#def inference(tensor_batch, model, classes_list, args):
 
-    output = torch.squeeze(torch.sigmoid(model(tensor_batch)))
-    np_output = output.cpu().detach().numpy()
-    idx_sort = np.argsort(-np_output)
+#    output = torch.squeeze(torch.sigmoid(model(tensor_batch)))
+#    np_output = output.cpu().detach().numpy()
+#    idx_sort = np.argsort(-np_output)
     # Top-k
-    detected_classes = np.array(classes_list)[idx_sort][: args.top_k]
-    scores = np_output[idx_sort][: args.top_k]
+#    detected_classes = np.array(classes_list)[idx_sort][: args.top_k]
+#    scores = np_output[idx_sort][: args.top_k]
     # Threshold
-    idx_th = scores > args.threshold
-    return detected_classes[idx_th], scores[idx_th]
+#    idx_th = scores > args.threshold
+#    return detected_classes[idx_th], scores[idx_th]
 
 
-def display_image(im, tags, filename, path_dest):
+#def display_image(im, tags, filename, path_dest):
 
-    if not os.path.exists(path_dest):
-        os.makedirs(path_dest)
+#    if not os.path.exists(path_dest):
+#        os.makedirs(path_dest)
 
-    plt.figure()
-    plt.imshow(im)
-    plt.axis('off')
-    plt.axis('tight')
-    plt.rcParams["axes.titlesize"] = 16
-    plt.title("Predicted classes: {}".format(tags))
-    print(os.path.join(path_dest, filename))
-    plt.savefig(os.path.join(path_dest, filename))
+#    plt.figure()
+#    plt.imshow(im)
+#    plt.axis('off')
+#    plt.axis('tight')
+#    plt.rcParams["axes.titlesize"] = 16
+#    plt.title("Predicted classes: {}".format(tags))
+#    print(os.path.join(path_dest, filename))
+#    plt.savefig(os.path.join(path_dest, filename))
 
 
-def main():
-    print('airbnb_reg demo of inference code on a single album.')
+def main(ids):
 
-    # ----------------------------------------------------------------------
-    # Preliminaries
     args = parser.parse_args()
 
-    # Setup model
-    print('creating and loading the model...')
-    state = torch.load(args.model_path, map_location='cpu')
-    # args.num_classes = state['num_classes']
-    model = create_model(args)#.cuda()
-    model.load_state_dict(state['model'], strict=True)
-    model.eval()
-    classes_list = np.array(list(state['idx_to_class'].values()))
-    print('Class list:', classes_list)
+    prefix = args.path_output
 
-    # Setup data loader
-    print('creating data loader...')
-    val_loader = create_dataloader(args)
-    print('done\n')
+    meaningful_images = pd.DataFrame(columns = ['ids', 'meaningful_im','is_first'])
 
-    # Get album
-    tensor_batch, montage = get_album(args)
+    for id in ids:
+        album_path = '{}{}_attn.pt'.format(prefix, id)
+        order_path = '{}{}_order.pt'.format(prefix, id)
+        album_attention = torch.load(album_path)
+        album_order = torch.load(order_path)
+        arg_max = int(torch.argmax(album_attention[0, 1:]))
+        meaningful_image = album_order[arg_max]
+        is_first = (meaningful_image[-1] == '0')
+        meaningful_images = pd.concat([meaningful_images, pd.DataFrame({"ids":[id], 'meaningful_im':[meaningful_image], "is_first":[is_first]})])
 
-    # Inference
-    tags, confs = inference(tensor_batch, model, classes_list, args)
 
-    # Visualization
-    display_image(montage, tags, 'result.jpg', os.path.join(args.path_output, args.album_path).replace("./albums", ""))
+    guessed_baseline = meaningful_images.is_first.sum()
+    guessed_baseline = guessed_baseline/len(ids)
 
-    # Actual validation process
-    # print('loading album and doing inference...')
-    # map = validate(model, val_loader, classes_list, args.threshold)
-    # print("final validation map: {:.2f}".format(map))
-
+    return meaningful_images, guessed_baseline
     print('Done\n')
 
 
 if __name__ == '__main__':
-    main()
+    ids = pd.read_csv('/Users/talsokolov/Desktop/airbnb_dlp/airbnb_reg/all_ids.csv')['id'].values
+    main(ids)
